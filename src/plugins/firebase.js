6ff47eb20a9267568dfe4b5c25381d14d1886951
Torrence Brown <torrenceb90@gmail.com>
2024-01-20 15:57:50 -0700

    Pull backend into project

---
+++ src/plugins/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDhCClGX2tshKPasYmxjFvH0zV59hmRhag",
+  authDomain: "portfolio-house.firebaseapp.com",
+  projectId: "portfolio-house",
+  storageBucket: "portfolio-house.appspot.com",
+  messagingSenderId: "192968111068",
+  appId: "1:192968111068:web:0b957f570fd0489803d519",
+  measurementId: "G-TGJXHHKH63",
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
